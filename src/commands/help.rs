8f3ad75c17021f1290e50fa7059926af8713c772
ElementalAlchemist <ElementAlchemist7@gmail.com>
2023-12-26 21:46:32 -0600

    Add help command

---
+++ src/commands/help.rs
@@ -0,0 +1,15 @@
+use crate::command_types::{CommandError, Context};
+use miette::IntoDiagnostic;
+use poise::builtins::HelpConfiguration;
+
+/// Gets command help
+#[poise::command(slash_command)]
+pub async fn help(ctx: Context<'_>, command: Option<String>) -> Result<(), CommandError> {
+	let config = HelpConfiguration {
+		ephemeral: true,
+		..Default::default()
+	};
+	poise::builtins::help(ctx, command.as_deref(), config)
+		.await
+		.into_diagnostic()
+}
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,6 +1,9 @@
 use crate::command_types::{CommandError, Data};
 use poise::Command;
 
+mod help;
+use help::help;
+
 mod partner_categories;
 use partner_categories::partner_categories;
 
@@ -14,5 +17,5 @@ mod setup;
 use setup::setup;
 
 pub fn get_all_commands() -> Vec<Command<Data, CommandError>> {
-	vec![partner_categories(), partners(), setup(), settings()]
+	vec![help(), partner_categories(), partners(), setup(), settings()]
 }
